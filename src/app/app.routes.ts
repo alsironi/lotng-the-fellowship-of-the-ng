949e29758cfb30ba63031e71cc96939d5c65b1a8
Alvaro Leon <hello@alsironidev.com>
2025-05-15 14:15:00 +0200

    feat: setup routing for homepage and character routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { GandalfComponent } from './characters/gandalf/gandalf.component';
+import { TheodenComponent } from './characters/theoden/theoden.component';
+import { FrodoComponent } from './characters/frodo/frodo.component';
+import { SamComponent } from './characters/sam/sam.component';
+import { AragornComponent } from './characters/aragorn/aragorn.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'gandalf', component: GandalfComponent },
+  { path: 'theoden', component: TheodenComponent },
+  { path: 'frodo', component: FrodoComponent },
+  { path: 'sam', component: SamComponent },
+  { path: 'aragorn', component: AragornComponent },
+  { path: '**', redirectTo: '' }
+];
